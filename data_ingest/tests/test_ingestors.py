49368b666d24ca0a8ce275ca4307e07dc28fa634
Amy Mok <amy.mok@gsa.gov>
2018-12-10 16:00:57 -0800

    Compliant with flake8

--- data_ingest/tests/test_ingestors.py
+++ data_ingest/tests/test_ingestors.py
@@ -23,14 +23,14 @@ class TestIngestors(SimpleTestCase):
                     "dollars_budgeted"
                   ]
                 }
-        row_dict = OrderedDict([('category', 'red tape'), 
-                                ('dollars_budgeted', '2000'), 
+        row_dict = OrderedDict([('category', 'red tape'),
+                                ('dollars_budgeted', '2000'),
                                 ('dollars_spent', '2300')])
 
         exp_result = {
             'severity': 'Error',
             'code': None,
             'message': "spending should not exceed budget",
-            'error_columns': [ 'dollars_budgeted', 'dollars_spent']
+            'error_columns': ['dollars_budgeted', 'dollars_spent']
         }
         self.assertEqual(row_validation_error(rule, row_dict), exp_result)
