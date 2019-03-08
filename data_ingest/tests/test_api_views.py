2c62fb34b5e6594979c0ed98260a28f54d4b0026
Amy Mok <amy.mok@gsa.gov>
2019-03-08 14:07:43 -0800

    Add test for reorder_csv

--- data_ingest/tests/test_api_views.py
+++ data_ingest/tests/test_api_views.py
@@ -1,6 +1,7 @@
-from data_ingest.api_views import to_tabular
 from django.test import SimpleTestCase
+from unittest.mock import patch
 
+from data_ingest.api_views import reorder_csv, to_tabular
 
 class TestJSONtoTable(SimpleTestCase):
 
@@ -21,3 +22,14 @@ class TestJSONtoTable(SimpleTestCase):
             for col, value in obj.items():
                 processed_column_index = result[0].index(col)
                 self.assertEqual(result[i+1][processed_column_index], value)
+
+class TestReorderCSV(SimpleTestCase):
+
+    @patch('data_ingest.api_views.get_ordered_headers')
+    def test_reorder_csv(self, mock_headers):
+        mock_headers.return_value = ['c', 'a', 'b']
+        data = {'source': b'a,b,c\n1,2,3\n4,5,6\n'}
+        self.assertEqual({'source': b'c,a,b\r\n3,1,2\r\n6,4,5\r\n'}, reorder_csv(data))
+
+        data = {'source': b''}
+        self.assertEqual({'source': b''}, reorder_csv(data))
