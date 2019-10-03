7e115d89ba1e5bc3c97873f49b6481f6057c9f37
Amy Mok <amy.mok@gsa.gov>
2019-10-03 15:30:49 -0700

    Change version number in setup (#75)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ReVAL',
-    version='0.4',
+    version='v0.6.0',
     packages=find_packages(),
     include_package_data=True,
     license='CC0-1.0',
