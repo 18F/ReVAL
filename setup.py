93b7d90a993c6bed1f876185d7062ccdd98a9bf4
James Tranovich <james.tranovich@gsa.gov>
2020-02-05 11:46:16 -0800

    bump up version number to v0.6.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ReVAL',
-    version='v0.6.1',
+    version='v0.6.2',
     packages=find_packages(),
     include_package_data=True,
     license='CC0-1.0',
