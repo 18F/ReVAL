eb719414798fa4671b97782d26f25bb40f6c1643
James Tranovich <james.tranovich@gsa.gov>
2020-02-26 16:14:54 -0800

    adjust settings for the other example projects

--- examples/p02_budgets/p02_budgets/settings.py
+++ examples/p02_budgets/p02_budgets/settings.py
@@ -106,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        # allow the UI to use the API
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated', )
